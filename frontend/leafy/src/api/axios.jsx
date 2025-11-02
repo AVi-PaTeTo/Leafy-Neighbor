eea4526207e846a9ba30f14f3d8c961c58612063
MushyKeycaps <abhi.soy.332@gmail.com>
2025-11-02 14:29:50 +0530

    prepared for deployment

--- frontend/leafy/src/api/axios.jsx
+++ frontend/leafy/src/api/axios.jsx
@@ -1,18 +1,18 @@
-import axios from "axios"
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
-})
+  baseURL: process.env.REACT_APP_API_BASE_URL + "/api/",
+});
 
 API.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem("access_token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-  
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default API;
