867865ea96c0349268edc36750eaa28bbffb9072
MushyKeycaps <abhi.soy.332@gmail.com>
2025-11-05 13:16:29 +0530

    dynamic log icon in sidebar

--- frontend/leafy/src/components/NavBar/NavSidebar.jsx
+++ frontend/leafy/src/components/NavBar/NavSidebar.jsx
@@ -118,7 +118,9 @@ const NavSidebar = (props) => {
                                 </div>
                                 <h2 className="text-xl font-semibold pt-0.5 text-gray-100">{user != null && user.username != ""? user.username :"Username"}</h2>
                             </div>
-                            <svg className="fill-red-500 mr-2" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#afc35f"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h292.66v66.67H186.67v586.66h292.66V-120H186.67Zm470.66-176.67-47-48 102-102H360v-66.66h351l-102-102 47-48 184 184-182.67 182.66Z"/></svg>
+                            {user == null ?
+                            <svg className="fill-primary mr-2" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#75FB4C"><path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/></svg> :
+                            <svg className="fill-red-500 mr-2" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#afc35f"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h292.66v66.67H186.67v586.66h292.66V-120H186.67Zm470.66-176.67-47-48 102-102H360v-66.66h351l-102-102 47-48 184 184-182.67 182.66Z"/></svg>}
                         </div>
                     </div>
                 </div>
