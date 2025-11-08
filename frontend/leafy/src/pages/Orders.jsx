de09f4a5fad246d12738c497c215f7efedad3f28
MushyKeycaps <abhi.soy.332@gmail.com>
2025-11-08 10:25:23 +0530

    added website screenshots for the readme

--- frontend/leafy/src/pages/Orders.jsx
+++ frontend/leafy/src/pages/Orders.jsx
@@ -96,7 +96,7 @@ const Order = (props) => {
                                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>
                                             </button>
                                             <span className="h-full w-33 shrink-0 flex flex-col justify-between">
-                                                <img className="h-[70%] object-cover self-start rounded-tl-md" src={order.order_items[0].product_image} alt="" />
+                                                <img className="h-[70%] object-cover self-start rounded-tl-md" src={order != undefined ? order.order_items[0].product_image:""} alt="" />
                                                 <div className="h-[30%] items-center flex p-2 text-gray-100 bg-zinc-800 rounded-bl-md ">
                                                     <h2 className=" text-[1.1rem] flex-1 pt-1">{status[order.status].title}</h2>
                                                     {status[order.status].icon}
